efbf4c27da644d3748e6d48ad8f7e9471e90685c
kozinaki <lakec3.14@yandex.ru>
2018-05-04 23:58:51 +0300

    Problem 1. Reformatted code.

--- projectEuler.net/src/problem001.c
+++ projectEuler.net/src/problem001.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 /**
-* Multiples of 3 and 5
-* Problem 1
-*
-* If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-* Find the sum of all the multiples of 3 or 5 below 1000.
-*/
+ * Multiples of 3 and 5
+ * Problem 1
+ *
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ */
 
 int main() {
-	int sum = 0;
-	for(int i = 1; i < 1000; i++)
-		if(i % 3 == 0 || i % 5 == 0)
-			sum += i;
-	printf("result: %i\n", sum);
-	return 0;
+    int sum = 0;
+    for(int i = 1; i < 1000; i++)
+        if(i % 3 == 0 || i % 5 == 0)
+            sum += i;
+    printf("result: %i\n", sum);
+    return 0;
 }
