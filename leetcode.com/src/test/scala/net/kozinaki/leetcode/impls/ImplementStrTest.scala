d8c985885579f137d1a30f51c5d0207ee2acff89
ji est <lakec3.14@yandex.ru>
2020-11-11 00:41:51 +0300

    Improved unit test.

--- leetcode.com/src/test/scala/net/kozinaki/leetcode/impls/ImplementStrTest.scala
+++ leetcode.com/src/test/scala/net/kozinaki/leetcode/impls/ImplementStrTest.scala
@@ -1,24 +1,24 @@
 package test.scala.net.kozinaki.leetcode.impls
 
-import main.scala.net.kozinaki.leetcode.impls.ImplementStr
+import main.scala.net.kozinaki.leetcode.impls.ImplementStr._
 import org.scalatest.funsuite.AnyFunSuite
 
 class ImplementStrTest extends AnyFunSuite {
 
   test("Return right index of substring form string") {
-    assert(ImplementStr.strStr("mississippi", "issip") == 4)
-    assert(ImplementStr.strStr("hello", "ll") == 2)
-    assert(ImplementStr.strStr("hello", "o") == 4)
-    assert(ImplementStr.strStr("he", "hello") == -1)
-    assert(ImplementStr.strStr("hello World", " ") == 5)
-    assert(ImplementStr.strStr("aaaaa", "bba") == -1)
-    assert(ImplementStr.strStr("aaaaa", "a") == 0)
-    assert(ImplementStr.strStr("abaaa", "b") == 1)
-    assert(ImplementStr.strStr("abaaa", "ba") == 1)
-    assert(ImplementStr.strStr("abaaa", "baaa") == 1)
-    assert(ImplementStr.strStr("abaaa", "baaaa") == -1)
-    assert(ImplementStr.strStr("", "") == 0)
-    assert(ImplementStr.strStr("text", "") == 0)
+    assert(strStr("mississippi", "issip") == 4)
+    assert(strStr("hello", "ll") == 2)
+    assert(strStr("hello", "o") == 4)
+    assert(strStr("he", "hello") == -1)
+    assert(strStr("hello World", " ") == 5)
+    assert(strStr("aaaaa", "bba") == -1)
+    assert(strStr("aaaaa", "a") == 0)
+    assert(strStr("abaaa", "b") == 1)
+    assert(strStr("abaaa", "ba") == 1)
+    assert(strStr("abaaa", "baaa") == 1)
+    assert(strStr("abaaa", "baaaa") == -1)
+    assert(strStr("", "") == 0)
+    assert(strStr("text", "") == 0)
   }
 
 }
